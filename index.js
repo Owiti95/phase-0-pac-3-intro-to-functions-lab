0566b3c05aed7b9a7b92595d13f3f4c983d2da7a
hannington owiti <hanningtonowiti@gmail.com>
2024-07-05 22:45:40 +0300

    phase-0-pac-3-intro-to-functions-lab

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(HELLO) {
+    return HELLO.toLowerCase();
+}
+
+
+function logShout(string) {
+    const shoutedString = string.toUpperCase();
+    console.log(shoutedString);
+  }
+
+
+function logWhisper(STRING) {
+    const whisper = STRING.toLowerCase();
+    console.log(whisper);
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    return "I can't hear you!";
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    return
+}
+
+
+  function sayHiToHeadphonedRoommate(inputString) {
+    if (inputString === "Let's have dinner together!") {
+        return "I would love to!";
+    }
+    else if (inputString === inputString.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    else if (inputString === inputString.toLowerCase()) {
+        return "I can't hear you!";
+    }
+  }
